e42fd874d705e2e2666ff8f382b2c085d97d55c4
Nauman Zubair <nthreadsblog@gmail.com>
2019-04-13 14:38:49 +0400

    Commands on master

--- MyScript.py
+++ MyScript.py
@@ -1,1 +1,4 @@
-#This is my first line on master
+# cd <directory> // go to directory of your project
+# git init  // This will initialize 
+# git add . // Add all files of current directory to git staging (temporary storage)
+# git commit -m "This is your first commit"
