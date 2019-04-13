fd1d9c5c2c09d140766a3484a87b3ac8307c0cbf
Nauman Zubair <nthreadsblog@gmail.com>
2019-04-13 14:39:49 +0400

    Commiting on develp

--- MyScript.py
+++ MyScript.py
@@ -1,5 +1,9 @@
-#This is my first line on master
+# cd <directory> // go to directory of your project
+# git init  // This will initialize 
+# git add . // Add all files of current directory to git staging (temporary storage)
+# git commit -m "This is your first commit"
 
-#Another line of code on develop branch
-
-#Adding new feature
+# git branch // check all your branches and selected branch with sterik
+# git branch develop // create 'develop' branch
+# git checkout develop // select develop branch
+# git commit -am "Add all changes to stagging and write message altogather"
